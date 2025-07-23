11c6161140373eb1079cb9997bcfcf039d479020
Roman Ludwig <48687784+rmnldwg@users.noreply.github.com>
2025-07-23 17:01:43 +0200

    test: test new combine/augment with CLB patient 17
    Related: #7

--- tests/test_infer_levels.py
+++ tests/test_infer_levels.py
@@ -1,36 +1,25 @@
 """Check that inferring sub- and super-levels works correctly."""
 
-import pandas as pd
-import pytest
-
 import lydata  # noqa: F401
+from lydata.augmentor import combine_and_augment_levels
+from lydata.utils import get_default_modalities
 
 
-@pytest.fixture
-def mock_data() -> pd.DataFrame:
-    """Create a mock dataset for testing."""
-    return pd.DataFrame({
-        ("MRI", "ipsi",   "Ia" ): [True , False, False, None, None ],
-        ("MRI", "ipsi",   "Ib" ): [False, True , False, None, False],
-        ("MRI", "contra", "IIa"): [False, False, None , None, None ],
-        ("MRI", "contra", "IIb"): [False, True , True , None, False],
-        ("CT",  "ipsi",   "I"  ): [True , False, False, None, None ],
-    })
-
-
-def test_infer_superlevels(mock_data: pd.DataFrame) -> None:
-    """Check that superlevels are inferred correctly."""
-    inferred = mock_data.ly.infer_superlevels(modalities=["MRI"])
-
-    expected_ipsi_I = [True, True, False, None, None]
-    expected_contra_II = [False, True, True, None, None]
-
-    for example in range(len(mock_data)):
-        assert (
-            inferred.iloc[example].MRI.ipsi.I
-            == expected_ipsi_I[example]
-        ), f"{example = } mismatch for ipsi I"
-        assert (
-            inferred.iloc[example].MRI.contra.II
-            == expected_contra_II[example]
-        ), f"{example = } mismatch for contra II"
+def test_augment_levels() -> None:
+    """Check the advanced combination and augmentation of diagnoses and levels."""
+    clb_raw = next(lydata.load_datasets(institution="clb", subsite="oropharynx"))
+    modalities = get_default_modalities()
+    modalities = {
+        name: mod
+        for name, mod in modalities.items()
+        if name in clb_raw.columns.get_level_values(0)
+    }
+    clb_aug = combine_and_augment_levels(
+        diagnoses=[clb_raw[mod] for mod in modalities.keys()],
+        specificities=[mod.spec for mod in modalities.values()],
+        sensitivities=[mod.sens for mod in modalities.values()],
+    )
+    assert len(clb_aug) == len(clb_raw), "Augmented data length mismatch"
+    assert clb_aug.iloc[16].ipsi.I == False
+    assert clb_aug.iloc[16].ipsi.Ia == False
+    assert clb_aug.iloc[16].ipsi.Ib == False
