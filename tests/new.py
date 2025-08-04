c89275db9964a0553baf26dbeeb601cb28211b40
Roman Ludwig <48687784+rmnldwg@users.noreply.github.com>
2025-08-04 17:38:34 +0200

    test: add scripts to compare augment/combine

---
+++ tests/new.py
@@ -0,0 +1,38 @@
+# /// script
+# requires-python = ">=3.10"
+# dependencies = [
+#     "lydata @ git+https://github.com/lycosystem/lydata-package@aff4c795710350ed4a8c3f146a89eb31b6fa8416",
+# ]
+# ///
+
+"""Get all datasets and enhance them using the new lydata package."""
+
+import pandas as pd
+from loguru import logger
+
+import lydata
+from lydata.accessor import LyDataFrame
+
+logger.enable("lydata")
+
+
+def main() -> None:
+    """Run the main function to load and enhance datasets."""
+    full_dataset: LyDataFrame = pd.DataFrame()
+    for dataset in lydata.load_datasets(
+        repo_name="lycosystem/lydata.private",
+        ref="ab04379a36b6946306041d1d38ad7e97df8ee7ba",
+    ):
+        full_dataset = pd.concat([full_dataset, dataset], ignore_index=True)
+        logger.info(f"Added {len(dataset)=} rows to the full dataset.")
+
+    enhanced_dataset = full_dataset.ly.enhance()
+    added_cols = enhanced_dataset.shape[1] - full_dataset.shape[1]
+    logger.info(f"Enhanced dataset has {added_cols} new columns.")
+
+    enhanced_dataset.to_csv("new.csv", index=False)
+    logger.success("Enhanced dataset saved to 'new.csv'.")
+
+
+if __name__ == "__main__":
+    main()
