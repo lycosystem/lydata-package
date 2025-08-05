fce13ba672103ec5e19f01140a275e7b045c3cd9
Roman Ludwig <48687784+rmnldwg@users.noreply.github.com>
2025-08-05 10:58:49 +0200

    test: check one patient with specific issue

--- tests/test_augmentor.py
+++ tests/test_augmentor.py
@@ -1,15 +1,29 @@
 """Check that inferring sub- and super-levels works correctly."""
 
+import pandas as pd
+import pytest
+
 import lydata  # noqa: F401
 from lydata.augmentor import combine_and_augment_levels
 from lydata.utils import get_default_modalities
 
 
-def test_clb_patient_17() -> None:
-    """Check the advanced combination and augmentation of diagnoses and levels."""
-    clb_raw = next(
-        lydata.load_datasets(year=2021, institution="clb", subsite="oropharynx")
+@pytest.fixture
+def clb_raw() -> pd.DataFrame:
+    """Load the CLB dataset."""
+    return next(
+        lydata.load_datasets(
+            year=2021,
+            institution="clb",
+            subsite="oropharynx",
+            use_github=True,
+            ref="4668ff6006764169411d6d198c126b020a7892b2",
+        ),
     )
+
+
+def test_clb_patient_17(clb_raw: pd.DataFrame) -> None:
+    """Check the advanced combination and augmentation of diagnoses and levels."""
     modalities = get_default_modalities()
     modalities = {
         name: mod
@@ -25,6 +39,15 @@ def test_clb_patient_17() -> None:
     assert clb_aug.iloc[16].ipsi.I == False
     assert clb_aug.iloc[16].ipsi.Ia == False
     assert clb_aug.iloc[16].ipsi.Ib == False
+
+
+def test_clb_patient_p011(clb_raw: pd.DataFrame) -> None:
+    """Check that this patient's `NaN` values are handled correctly."""
+    idx = clb_raw.ly.id == "P011"
+    patient = clb_raw.loc[idx]
+    enhanced = patient.ly.enhance()
+
+    assert enhanced.iloc[0].pathology.ipsi.II == patient.iloc[0].pathology.ipsi.II
 
 
 def test_usz_patient_9() -> None:
